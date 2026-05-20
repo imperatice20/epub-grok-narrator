cb36942d5221aba154820203d5868dc2f8462210
imperatice20 <imperatice20@gmail.com>
2026-05-19 22:37:48 -0500

    Create package __init__.py with version

---
+++ src/epub_grok_narrator/__init__.py
@@ -0,0 +1,10 @@
+"""epub-grok-narrator: Convert EPUB to audio narrations for Grok Voice and more."""
+
+__version__ = "0.1.0"
+
+# from .parser import EPUBParser
+# from .chunker import TextChunker
+# from .tts import TTSGenerator
+# from .exporter import AudioExporter
+
+# __all__ = ["EPUBParser", "TextChunker", "TTSGenerator", "AudioExporter"]
